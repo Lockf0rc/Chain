77b9bf5083a99ef0a02145638adc61847912740c
miguel <metuxxman@gmail.com>
2015-04-11 03:24:40 -0700

    Email error reporting to email specifide  set_error_handler:calls functintion when an error occurs  error_log($msg,1,emailname,[header])
     doconnect: connects to the db based on the constants provided in the
     interface of IConnect. and  reports error if it cannot connect.
     returns mysqli_connect object.

---
+++ UConnect.php
@@ -0,0 +1,29 @@
+<?php
+/*
+FILENAME:UConnect.php
+SCOPE:$DATABase
+*/
+#custome error handler with emails 
+set_error_handler('error_mail');
+function error_mail($number,$message,$file,$line,$vars){
+$email="<p>An Error ($number) occurred on line <strong>$line</strong>
+<strong>file: $file.</strong></p><p>message: $message </p>";
+$email.="<pre>".print_r($vars,1). "</pre>";
+$headers = 'Content-type: text/html; charset=utf-8'."\r\n";
+error_log($mail,1,"error@scriptreference.com",$headers);
+}
+Class UConnect implements IConnect
+{
+	private static $server= IConnect::HOST;
+	private static $currentDB= IConnect::DBNAME;
+	private static $user= IConnect::UNAME;
+	private static $pass= IConnect::PW;
+	private static $hookup;
+
+	/*implements*/
+	public function doConnect(){
+		self::$hookup=mysqli_connect(self::$server,self::$user,self::$pass,self::$hookup);
+
+	}
+}
+
